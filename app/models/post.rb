ac1d1b134406a8f5d5bcc3fcfabd235d0c97e769
Harrison Njuguna <noqsnoqs@gmail.com>
2023-03-29 14:10:52 +0300

    Modify models after review

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  after_create :update_posts_counter
+  after_save :update_posts_counter
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User'
