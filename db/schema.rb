6b5aa8b0f4d67eadc1dacd1341fc450013981827
Harrison Njuguna <noqsnoqs@gmail.com>
2023-03-29 13:00:49 +0300

    Add models

--- db/schema.rb
+++ db/schema.rb
@@ -10,52 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_080452) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_130417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "AuthorId"
-    t.integer "PostId"
-    t.text "Text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["AuthorId"], name: "index_comments_on_AuthorId"
-    t.index ["PostId"], name: "index_comments_on_PostId"
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer "AuthorId"
-    t.integer "PostId"
+  create_table "likes", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["AuthorId"], name: "index_likes_on_AuthorId"
-    t.index ["PostId"], name: "index_likes_on_PostId"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "AuthorId"
-    t.string "Title"
-    t.text "Text"
-    t.integer "CommentCounter"
-    t.integer "LikeCounter"
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["AuthorId"], name: "index_posts_on_AuthorId"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "Name"
-    t.text "Bio"
-    t.integer "PostCounter"
-    t.string "Image"
+    t.string "name"
+    t.string "photo"
+    t.string "bio"
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts", column: "PostId"
-  add_foreign_key "comments", "users", column: "AuthorId"
-  add_foreign_key "likes", "posts", column: "PostId"
-  add_foreign_key "likes", "users", column: "AuthorId"
-  add_foreign_key "posts", "users", column: "AuthorId"
 end
