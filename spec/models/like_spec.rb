9df994082788833b92cab45f709022509c946634
Harrison Njuguna <noqsnoqs@gmail.com>
2023-03-31 15:02:01 +0300

    Modify models

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  subject { Like.new(post_id: 9) }
+
+  before { subject.save }
+
+  it 'The post_id should be present' do
+    subject.post_id.nil?
+    expect(subject).to_not be_valid
+  end
+
+  it 'The post_id should be an integer' do
+    subject.post_id = '15'
+    expect(subject).to_not be_valid
+  end
+end
