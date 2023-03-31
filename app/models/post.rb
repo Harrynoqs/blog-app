90434070249be4fb07e6d09ca904cacbab9dae94
Harrison Njuguna <noqsnoqs@gmail.com>
2023-03-31 15:56:17 +0300

    Clear linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,5 +17,4 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
-  
 end
