03b0c597bbaf23f66ea8a8198c0be160cc86e24e
Harrison Njuguna <noqsnoqs@gmail.com>
2023-04-05 17:18:04 +0300

    Modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  def latest_posts
+  def recent_posts
     Post.where(author: self).limit(3)
   end
 
